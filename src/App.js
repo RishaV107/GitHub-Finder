246bd065cdac79fada8059ea68b32a80bde186e4
rishav.singh <risingh@ingenuitygaming.com>
2021-05-25 10:40:58 +0530

    Implemented the features searchUsers,ClearUsers , alert message for empty search-box

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import User from "./components/user/Users";
+import axios from "axios";
+import Search from "./components/user/Search";
+import Alert from "./components/layout/Alert";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${"dd3f8df5a995af1ff36b"}&client_secret=${"127650d7358b381a217e7ec117b28e1772bad25d"}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+  //Search GitHub users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${"dd3f8df5a995af1ff36b"}&client_secret=${"127650d7358b381a217e7ec117b28e1772bad25d"}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <User />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <User loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/user/Users.js
+++ src/components/user/Users.js
@@ -1,40 +1,26 @@
-import React, { Component } from "react";
-import UserItems from "../layout/user/UserItems";
+import React from "react";
+import UserItems from "../user/UserItems";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: 2,
-        login: "defunkt",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        id: 3,
-        login: "pjhyett",
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItems key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
